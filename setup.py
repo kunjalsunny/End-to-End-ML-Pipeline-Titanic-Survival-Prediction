78be4580e154bf0feb41dc8ceb0b220ad0f3a443
kunjalsunny <kunjalmahant72@gmail.com>
2025-12-26 15:05:12 -0500

    First Setup commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+    return requirements
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Kunjal",
+    author_email="abc123@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
